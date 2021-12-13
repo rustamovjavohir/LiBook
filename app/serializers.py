2d36465db880967ad7059c1bb10f4006cdcf999b
Akbar Haydarov <81344759+Akbarpython@users.noreply.github.com>
2021-12-14 00:24:17 +0500

    update
    update

--- app/serializers.py
+++ app/serializers.py
@@ -5,3 +5,18 @@ class UserSerializers(ModelSerializer):
     class Meta:
         model = Users
         fields = "__all__"
+class BookSerializers(ModelSerializer):
+    class Meta:
+        model = Book
+        fields = "__all__"
+
+
+class BoxSerializers(ModelSerializer):
+    class Meta:
+        model = Box
+        fields = "__all__"
+
+class MessageSerializers(ModelSerializer):
+    class Meta:
+        model = Message
+        fields = "__all__"
